1ca32b21b2fde92ebd0388c191010b43f805f1fe
Gabriel Alecu <gc.alecu@gmail.com>
2016-11-12 11:40:29 +0200

    Project Init

---
+++ src/main/java/com/badomega/HospitalInfectionsMonitoringSystemApplication.java
@@ -0,0 +1,12 @@
+package com.badomega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalInfectionsMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalInfectionsMonitoringSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/badomega/HospitalInfectionsMonitoringSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.badomega;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HospitalInfectionsMonitoringSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
