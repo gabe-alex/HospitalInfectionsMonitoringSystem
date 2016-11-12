455a68ef4b38a0c27b4e4b401b01f59e9e42be9d
Gabriel Alecu <gc.alecu@gmail.com>
2016-11-12 15:45:41 +0200

    Heroku fixes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar $JAVA_OPTS -Dserver.port=$PORT target/hims-0.0.1-SNAPSHOT.jar
--- src/test/java/com/badomega/hims/HospitalInfectionsMonitoringSystemApplicationTests.java
+++ src/test/java/com/badomega/hims/HospitalInfectionsMonitoringSystemApplicationTests.java
@@ -2,10 +2,13 @@ package com.badomega.hims;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
+@WebAppConfiguration
 @SpringBootTest
 public class HospitalInfectionsMonitoringSystemApplicationTests {
 
