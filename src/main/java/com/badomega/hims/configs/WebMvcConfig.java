b2140a5604f0a2c63db6e12bfdbe1f303cfd2d5f
MelaniaNitu <melania.nitu@yahoo.com>
2016-11-16 00:29:08 +0200

    added infections monitoring menu

--- src/main/java/com/badomega/hims/configs/WebMvcConfig.java
+++ src/main/java/com/badomega/hims/configs/WebMvcConfig.java
@@ -16,6 +16,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/users").setViewName("users");
         registry.addViewController("/beacons").setViewName("beacon");
         registry.addViewController("/persons").setViewName("persons");
+        registry.addViewController("/monitoring").setViewName("monitoring");
         registry.addViewController("/about").setViewName("about-page");
         registry.addViewController("/contact").setViewName("contact");
     }
