63d37f1cf2b0151ea2700b85f153bf78bb6df845
Gabriel Alecu <gc.alecu@gmail.com>
2016-11-18 21:55:38 +0200

    labels change

--- src/main/resources/static/js/monitoring.js
+++ src/main/resources/static/js/monitoring.js
@@ -9,10 +9,10 @@ himsApp.controller('DbController', function DbController($scope, $http, uiGridCo
     };
 
     $scope.gridOptions.columnDefs = [
-        { name: 'interaction.self.name', displayName: 'User 1', enableHiding: false},
-        { name: 'interaction.targetPerson.name', displayName: 'User 2', enableHiding: false},
-        { name: 'selfToTargetDiseases', displayName: 'Diseases U1 -> U2', enableHiding: false, cellFilter: 'diseasesDisplay'},
-        { name: 'targetToSelfDiseases', displayName: 'Diseases U2 -> U1', enableHiding: false, cellFilter: 'diseasesDisplay'},
+        { name: 'interaction.self.name', displayName: 'User', enableHiding: false},
+        { name: 'interaction.targetPerson.name', displayName: 'Other User', enableHiding: false},
+        { name: 'selfToTargetDiseases', displayName: 'To', enableHiding: false, cellFilter: 'diseasesDisplay'},
+        { name: 'targetToSelfDiseases', displayName: 'From', enableHiding: false, cellFilter: 'diseasesDisplay'},
         { name: 'interaction.startTimestamp', displayName: 'Meeting Start Time', enableHiding: false, cellFilter: "date : 'HH:mm:ss, dd.MM.yyyy' : '+0300'"},
         { name: 'interaction.endTimestamp', displayName: 'Meeting End Time', enableHiding: false, cellFilter: "date : 'HH:mm:ss, dd.MM.yyyy' : '+0300'"}
     ];
